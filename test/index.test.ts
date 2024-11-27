647ef3952f2f193817d98d065b325c337ce4fc0c
osawa-koki <94336223+osawa-koki@users.noreply.github.com>
2024-11-27 23:51:17 +0900

    サンプルテストの修正。

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,12 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 
-// import * as Infra from '../lib/index';
+import * as IndexStack from '../lib/index';
 
 test('Hoge Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Infra.InfraStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+  const app = new cdk.App();
+  const stack = new IndexStack.IndexStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.toJSON();
 });
